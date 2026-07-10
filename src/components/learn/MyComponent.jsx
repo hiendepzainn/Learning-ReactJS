bc0a36b7fe8387b9a6041e24ff923b5f82ee5e8a
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-07-10 16:04:11 +0700

    video31: use variable in HTML

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,15 @@
 import "./style.css";
 
 const MyComponent = () => {
+  // const variable = 12;
+  // const variable = "Hello princess";
+  // const variable = [1, 2, 3];
+  const variable = { name: "Hien", age: 23 };
   return (
     <>
       <div>hello hehehe, this is MyComponent</div>
-      <div className="abc">second line!</div>
+      <div className="abc">{JSON.stringify(variable)}</div>
+      <div>{console.log("hehe")}</div>
     </>
   );
 };
