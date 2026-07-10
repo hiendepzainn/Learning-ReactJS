777578a1fed2d630888d78d8d045353ef85a5e80
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-07-10 15:45:59 +0700

    video30: fragment + css + className

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,12 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return <div>hello hehehe, this is MyComponent</div>;
+  return (
+    <>
+      <div>hello hehehe, this is MyComponent</div>
+      <div className="abc">second line!</div>
+    </>
+  );
 };
 
 export default MyComponent;
