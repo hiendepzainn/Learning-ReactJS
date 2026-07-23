c061bb6bcd21207dcceae6902788dad23496d656
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-07-23 21:24:08 +0700

    video74: transfer INPUTs into MODAL

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, notification } from "antd";
+import { Button, Input, Modal, notification } from "antd";
 import axios from "axios";
 import { useState } from "react";
 import { createUserAPI } from "../../services/api.service";
@@ -8,14 +8,23 @@ const UserForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleClick = async () => {
+  const resetValue = () => {
+    setEmail("");
+    setName("");
+    setPassword("");
+    setPhone("");
+  };
+
+  const handleOk = async () => {
     const res = await createUserAPI(name, email, password, phone);
     if (res.data) {
       notification.success({
         message: "create user",
         description: "Tạo mới User thành công!",
       });
+      resetValue();
     } else {
       notification.error({
         message: "Error",
@@ -24,6 +33,15 @@ const UserForm = () => {
     }
 
     console.log("Response:", res);
+    setIsModalOpen(false);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
   };
   return (
     <>
@@ -31,50 +49,71 @@ const UserForm = () => {
         style={{
           display: "flex",
           justifyContent: "center",
-          margin: "20px 0px",
+          margin: "25px 0px",
         }}
       >
-        <div style={{ width: "97vw" }}>
-          <div>
-            <div style={{ marginBottom: "12px" }}>
-              <div>Full name</div>
+        <div
+          style={{
+            width: "97vw",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <h2>Table Users</h2>
+          <Button type="primary" onClick={showModal}>
+            Create User
+          </Button>
+          <Modal
+            title="Create User"
+            closable={{ "aria-label": "Custom Close Button" }}
+            open={isModalOpen}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            maskClosable={false}
+            okText="Create"
+          >
+            <div style={{ width: "97%" }}>
               <div>
-                <Input value={name} onChange={(e) => setName(e.target.value)} />
+                <div style={{ marginBottom: "10px" }}>
+                  <div>Full name</div>
+                  <div>
+                    <Input
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                  <div>Email</div>
+                  <div>
+                    <Input
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                  <div>Password</div>
+                  <div>
+                    <Input.Password
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                  <div>Phone number</div>
+                  <div>
+                    <Input
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-            <div style={{ marginBottom: "10px" }}>
-              <div>Email</div>
-              <div>
-                <Input
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-            </div>
-            <div style={{ marginBottom: "10px" }}>
-              <div>Password</div>
-              <div>
-                <Input.Password
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-            </div>
-            <div style={{ marginBottom: "10px" }}>
-              <div>Phone number</div>
-              <div>
-                <Input
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <Button type="primary" onClick={handleClick}>
-              Create User
-            </Button>
-          </div>
+          </Modal>
         </div>
       </div>
     </>
