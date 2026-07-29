e3a4dc96342d2c10a596516068e7a120399e4b1a
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-07-29 09:12:54 +0700

    video89: design Form REGISTER using <Form></Form> (uncontrolled-component)

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,58 @@
+import { Button, Form, Input } from "antd";
+
 const RegisterPage = () => {
+  const onFinishSuccess = (data) => {
+    console.log("Success!, data:", data);
+  };
+  const onFinishFailed = (dataFailed) => {
+    console.log("Failed, errorInfor:", dataFailed);
+  };
+
   return (
     <>
-      <div>Register Page hehe</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "25px 0px",
+        }}
+      >
+        <div style={{ width: "97%" }}>
+          <Form
+            layout="vertical"
+            onFinish={onFinishSuccess}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Full name"
+              name="fullName"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item label="Email" name="email" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true }]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              label="Phone number"
+              name="phone"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form>
+        </div>
+      </div>
     </>
   );
 };
