aa1eb7a1c4454164f389c1d743e4041b44722b2b
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-08-04 10:14:13 +0700

    video108: config RULES for <Form.Item></Form.Item>

--- src/components/book/book.form.modal2.jsx
+++ src/components/book/book.form.modal2.jsx
@@ -1,12 +1,24 @@
-import { Form, Input, Modal } from "antd";
+import { Form, Input, Modal, Select } from "antd";
 
 const FormModalUnControl = (props) => {
   const { isModalOpen, setIsModelOpen } = props;
 
-  const handleOk = () => {};
+  const [form] = Form.useForm();
+
+  const handleOk = () => {
+    form.submit();
+  };
 
   const handleCancel = () => {
     setIsModelOpen(false);
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
   };
   return (
     <>
@@ -17,21 +29,99 @@ const FormModalUnControl = (props) => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <Form layout="vertical">
-          <Form.Item label="Tiêu đề" name="mainText">
+        <Form
+          form={form}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          layout="vertical"
+        >
+          <Form.Item
+            label="Tiêu đề"
+            name="mainText"
+            rules={[
+              {
+                required: true,
+                message: "Vui lòng không được để trống!",
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Tác giả" name="author">
+          <Form.Item
+            label="Tác giả"
+            name="author"
+            rules={[
+              {
+                required: true,
+                message: "Vui lòng không được để trống!",
+              },
+              {
+                pattern: /^[a-zA-ZÀ-ỹ\s]+$/,
+                message: "Chỉ được phép điền ký tự chữ!",
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Giá tiền" name="price">
+          <Form.Item
+            label="Giá tiền (đ)"
+            name="price"
+            rules={[
+              {
+                required: true,
+                message: "Vui lòng không được để trống!",
+              },
+              {
+                pattern: /^\d+$/,
+                message:
+                  "Chỉ được phép điền chữ số và không chứa khoảng trắng!",
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Số lượng" name="quantity">
+          <Form.Item
+            label="Số lượng"
+            name="quantity"
+            rules={[
+              {
+                required: true,
+                message: "Vui lòng không được để trống!",
+              },
+              {
+                pattern: /^\d+$/,
+                message:
+                  "Chỉ được phép điền chữ số và không chứa khoảng trắng!",
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Thể loại" name="category">
-            <Input />
+          <Form.Item
+            label="Thể loại"
+            name="category"
+            rules={[
+              {
+                required: true,
+                message: "Vui lòng không được để trống!",
+              },
+            ]}
+          >
+            <Select
+              style={{ width: "100%" }}
+              options={[
+                { value: "Arts", label: "Arts" },
+                { value: "Business", label: "Business" },
+                { value: "Comics", label: "Comics" },
+                { value: "Cooking", label: "Cooking" },
+                { value: "Entertainment", label: "Entertainment" },
+                { value: "History", label: "History" },
+                { value: "Music", label: "Music" },
+                { value: "Sports", label: "Sports" },
+                { value: "Teen", label: "Teen" },
+                { value: "Travel", label: "Travel" },
+              ]}
+            />
           </Form.Item>
         </Form>
       </Modal>
